e911b984806de4533bd6e633db90394eee7ca365
Katie Ho <kho@thoughtworks.com>
2020-04-29 09:27:08 -0400

    [department-of-veterans-affairs/covid19-chatbot#170] Refactors coronavirus chatbot component to use ReactWebChat and npm version of botframework
    Co-authored-by: Shefali Nayak <shefali.nayak@thoughtworks.com>

--- src/applications/coronavirus-chatbot/createCoronavirusChatbot.js
+++ src/applications/coronavirus-chatbot/createCoronavirusChatbot.js
@@ -1,7 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import initializeChatbot from './index';
 import recordEvent from 'platform/monitoring/record-event';
 import { GA_PREFIX } from './utils';
 
-export default (_store, widgetType) => {
+export default (store, widgetType) => {
   // Derive the element to render our widget.
   const root = document.querySelector(`[data-widget-type="${widgetType}"]`);
 
@@ -9,24 +13,36 @@ export default (_store, widgetType) => {
   if (!root) {
     return;
   }
+  // webpackChunkName: "chatbot"
+  import('./chatbot-entry').then(module => {
+    const { CoronavirusChatbot } = module.default;
+    initializeChatbot()
+      .then(webchatOptions => {
+        recordEvent({
+          event: `${GA_PREFIX}-connection-successful`,
+          'error-key': undefined,
+        });
+        recordEvent({
+          event: `${GA_PREFIX}-load-successful`,
+          'error-key': undefined,
+        });
 
-  import(/* webpackChunkName: "chatbot" */ './index')
-    .then(module => {
-      const initializeChatbot = module.default;
-      initializeChatbot(root);
-    })
-    // eslint-disable-next-line no-unused-vars
-    .then(res => {
-      recordEvent({
-        event: `${GA_PREFIX}-load-successful`,
-        'error-key': undefined,
+        ReactDOM.render(
+          <Provider store={store}>
+            <CoronavirusChatbot config={webchatOptions} />
+          </Provider>,
+          root,
+        );
+      })
+      .catch(() => {
+        recordEvent({
+          event: `${GA_PREFIX}-connection-failure`,
+          'error-key': 'XX_failed_to_start_chat',
+        });
+        recordEvent({
+          event: `${GA_PREFIX}-load-failure`,
+          'error-key': undefined,
+        });
       });
-    })
-    // eslint-disable-next-line no-unused-vars
-    .catch(error => {
-      recordEvent({
-        event: `${GA_PREFIX}-load-failure`,
-        'error-key': undefined,
-      });
-    });
+  });
 };
